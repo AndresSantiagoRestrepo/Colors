ba7d8bc6911f022be6469fec8f10fef5649d190d
Stokker <stokkerma@gmail.com>
2023-02-21 11:40:35 -0500

    creacion de interfaz con layouts y contraints

---
+++ app/src/main/java/com/example/colors/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.colors;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.colors.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
